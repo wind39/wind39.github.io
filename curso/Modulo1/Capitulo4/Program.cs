cea143a50936721f8d9b24658095ac7f3df8a113
wind39 <william.ivanski@gmail.com>
2015-08-22 17:24:29 -0300

    M1 C4 P1

---
+++ curso/Modulo1/Capitulo4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Capitulo4
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //int x;
+            int n;
+
+            Console.Write("Digite o valor de N: ");
+            n = int.Parse(Console.ReadLine());
+
+            /*x = 1; // inicialização
+            do
+            {
+                Console.WriteLine("Dentro do laço, x = {0}", x); // código
+                x = x + 1; // incremento
+            }
+            while (x <= n); // condição de parada*/
+
+            /*x = 1; // inicialização
+            while (x <= n) // condição de parada
+            {
+                Console.WriteLine("Dentro do laço, x = {0}", x); // código
+                x = x + 1; // incremento
+            }*/
+
+            for (int x = 1; x <= n; x++)
+                Console.WriteLine("Dentro do laço, x = {0}", x);
+
+            //Console.WriteLine("Acabou o laço, x = {0}", x);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ curso/Modulo2/Modulo2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Modulo2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            DateTime d1, d2;
+            TimeSpan t;
+
+            d2 = DateTime.Now;
+            Console.WriteLine("Data/Hora atual: {0}", d2);
+
+            Console.Write("Qual a sua data de nascimento? ");
+            d1 = DateTime.Parse(Console.ReadLine());
+
+            t = d2 - d1;
+            Console.WriteLine("Você tem {0} anos.", t.Days / 365);
+            Console.WriteLine("Ou, mais especificamente, você tem {0} anos.", t.TotalDays / 365);
+
+            Console.ReadKey();
+        }
+    }
+}
