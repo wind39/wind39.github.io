07e88c05b328ea48d77440647042803b583bcf31
wind39 <william.ivanski@gmail.com>
2015-09-04 00:56:31 -0300

    Atualizando apresentação

--- curso/Modulo2/Modulo2/Program.cs
+++ curso/Modulo2/Modulo2/Program.cs
@@ -6,104 +6,14 @@ namespace Modulo2
     {
         public static void Main(string[] args)
         {
-            bool deu_problema;
+            System.IO.StreamWriter v_writer;
 
-            do
-            {
-                try
-                {
-                    Funcao();
-                    deu_problema = false;
-                }
-                catch (System.FormatException)
-                {
-                    Console.WriteLine("Erro! Problema no Parse...");
-                    deu_problema = true;
-                }
-                catch (System.DivideByZeroException)
-                {
-                    Console.WriteLine("Erro! Divisão por zero...");
-                    deu_problema = true;
-                }
-                catch (NumeroNegativoException e)
-                {
-                    Console.WriteLine(e.Texto);
-                    deu_problema = true;
-                }
-                catch (System.Exception e)
-                {
-                    Console.WriteLine("Erro! Aconteceu algum problema que eu não sei qual é...");
-                    Console.WriteLine(e.GetType().ToString() + " - " + e.Message);
-                    deu_problema = true;
-                }
-                finally
-                {
-                    Console.WriteLine("Entrei no finally!!");
-                }
-            }
-            while (deu_problema);
+            v_writer = new System.IO.StreamWriter("teste.txt");
 
-            Console.WriteLine("O programa continuou.");
+            v_writer.WriteLine("isso é um teste.");
+            v_writer.WriteLine("hehe");
 
-            Console.ReadKey();
-        }
-
-        public static void Funcao()
-        {
-            int a, b;
-
-            Console.Write("Digite o valor de A: ");
-            a = int.Parse(Console.ReadLine());
-
-            Console.Write("Digite o valor de B: ");
-            b = int.Parse(Console.ReadLine());
-
-            if (b < 0)
-                throw new NumeroNegativoException();
-
-            Console.WriteLine("{0} / {1} = {2}", a, b, a / b);
-        }
-
-        public static int LeA()
-        {
-            int x = 0;
-            bool digitou_errado;
-
-            do
-            {
-                try
-                {
-                    Console.Write("Digite o valor de A: ");
-                    x = int.Parse(Console.ReadLine());
-                    digitou_errado = false;
-                }
-                catch (System.FormatException)
-                {
-                    Console.WriteLine("Erro! Número inteiro digitado incorretamente.");
-                    digitou_errado = true;
-                }
-                catch (System.Exception)
-                {
-                    Console.WriteLine("Erro desconhecido!");
-                    digitou_errado = true;
-                }
-            }
-            while (digitou_errado);
-
-            return x;
-        }
-
-        public static int LeB()
-        {
-            int y;
-
-            Console.Write("Digite o valor de B: ");
-            y = int.Parse(Console.ReadLine());
-
-            if (y < 0)
-                throw new NumeroNegativoException();
-
-            return y;
+            v_writer.Close();
         }
     }
 }
