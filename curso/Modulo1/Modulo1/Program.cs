bdc7f715f959fbdaa77bc67b4194802380f7e7bb
wind39 <william.ivanski@gmail.com>
2015-09-12 03:06:46 -0300

    M1 A1

--- curso/Modulo1/Modulo1/Program.cs
+++ curso/Modulo1/Modulo1/Program.cs
@@ -21,27 +21,27 @@ namespace Modulo1
                         i - é a contagem de casas dentro do binário.
                         soma -
                         */
-            int k, a, b, x, n, i, soma;
+            //int k, a, b, x, n, i, soma;
 
             /*Variáveis para interromper o processo.
                         achou - momento em que sobra apenas a maior casa do binário;
                         prox - entra no início do próximo laço de repetição, para garantir que o laço será executada uma última vez para aquela maior casa do binário.
                         */
-            bool achou, prox;
+            //bool achou, prox;
 
             //Solicita ao usuário o número binário.
             Console.WriteLine ();
             Console.WriteLine ("Digite um número binário (apenas 1 ou 0).");
-            k = int.Parse (Console.ReadLine ());
+            //k = int.Parse (Console.ReadLine ());
 
             //Declarações iniciais.
-            a = k;
-            soma = 0;
-            x = 1;
-            achou = false;
-            prox = false;
+            //a = k;
+            //soma = 0;
+            //x = 1;
+            //achou = false;
+            //prox = false;
 
-            a = k;
+            /*a = k;
             soma = 0;
             x = 1;
             while (a > 0)
@@ -51,27 +51,22 @@ namespace Modulo1
 
                 a /= 10;
                 x *= 2;
-            }
-
-            //Processo que calcula o binário.
-            /*for (i = 1; !prox; i++) {
-                if (achou) {
-                    prox = true;
-                }
-                b = a % 10;
-                a = a / 10;
-                soma += (b * x);
-                x = 1;
-                for (n = 1; (n <= i); n++) {
-                    x *= 2;
-                }
-                if (a == 1) {
-                    achou = true;
-                }
             }*/
 
-            //Resposta.
-            Console.WriteLine("{0} = {1}", k, soma);
+            System.ConsoleKeyInfo t;
+            string binario = "";
+            int soma = 0;
+            do
+            {
+                t = Console.ReadKey();
+                if (t.KeyChar == '0' || t.KeyChar == '1')
+                {
+                    binario += t.KeyChar;
+                    soma = (soma * 2) + int.Parse(t.KeyChar.ToString());
+                }
+            }
+            while ((t.KeyChar == '0' || t.KeyChar == '1'));
+            Console.WriteLine("{0} = {1}", binario, soma);
 
             //Finalização do exercício.
             Console.WriteLine ();
